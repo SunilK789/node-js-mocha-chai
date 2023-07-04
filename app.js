4ff92a483a45e8086ccec34831d98c73b426f80c
Sunil Kumar <sunilpalarya@gmail.com>
2023-07-04 12:01:43 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+require("./model/db");
+
+app.use(cors);
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//route
+app.use('/api', require("./router/user"));
+
+app.get('/home', (req, res) => {
+    res.send('Hi this is first API');
+});
+
+app.listen(port, (req, res)=>{
+    console.log("Server Connected to port: ", port);
+});
---
+++ router/user.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+
+const {getUser} = require("../controller/user");
+
+router.get('/user', )
+module.exports = router;
